76c3f5768d764ddccd2b0c7174dc4d2e81aea7cd
cestorre <tu-email-de-github@ejemplo.com>
2026-04-13 18:57:50 -0400

    Guardando cambios antes del rebase

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,35 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json=myobj, headers=header)
+    
+    # Task 3: Formatear la respuesta
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        dominant_emotion = max(emotions, key=emotions.get)
+        
+        return {
+            'anger': anger,
+            'disgust': disgust,
+            'fear': fear,
+            'joy': joy,
+            'sadness': sadness,
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        # En caso de error o texto vacío
+        return {
+            'anger': None, 'disgust': None, 'fear': None, 
+            'joy': None, 'sadness': None, 'dominant_emotion': None
+        }
